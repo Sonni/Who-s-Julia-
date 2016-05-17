6cf036a2360c7258ba805438c311dc59df34b189
Sonni <Sonni@Qt-mc.local>
2016-05-17 10:33:17 +0200

    update

---
+++ Notes-Sonni/script.sh
@@ -0,0 +1,3 @@
+./quicksort dualquicksort bob.txt
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia test.txt
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia Euler11.jl input.txt input2.txt
