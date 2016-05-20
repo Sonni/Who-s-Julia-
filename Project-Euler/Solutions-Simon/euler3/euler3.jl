c0c7185ebba48c10cab759b1311759269140223c
simkn15 <simonknudsen89@gmail.com>
2016-05-20 09:11:26 +0200

    terminal input

--- Project-Euler/Solutions-Simon/euler3/euler3.jl
+++ Project-Euler/Solutions-Simon/euler3/euler3.jl
@@ -2,13 +2,12 @@
   The prime factors of 13195 are 5, 7, 13 and 29.
   What is the largest prime factor of the number 600851475143.
 =#
-function printsum(a)
-    # summary generates a summary of an object
-    println(repr(a))
-end
+#Answer: 6857
 
+#Terminal: Julia euler3.jl 600851475143
+limit = parse(Int, ARGS[1])
 function prime_factors(n)
-  factors = Int64[]
+  factors = Int[]
   d = 2
   while n > 1
     while n % d == 0
@@ -17,7 +16,7 @@ function prime_factors(n)
     end
     d += 1
   end
-  printsum(factors)
+  return factors[end]
 end
 
-prime_factors(600851475143)
+println(prime_factors(limit))
