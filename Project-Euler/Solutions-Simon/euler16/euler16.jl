f7d5c381a6f3849736ae51888e2a5ae8ab595918
simkn15 <simonknudsen89@gmail.com>
2016-05-20 12:43:45 +0200

    euler16 -> terminal

--- Project-Euler/Solutions-Simon/euler16/euler16.jl
+++ Project-Euler/Solutions-Simon/euler16/euler16.jl
@@ -5,35 +5,36 @@
 
 num = parse(Int, ARGS[1])
 power = parse(Int, ARGS[2])
-sum = 0
 
-#arr = [2]
-arr = zeros(Int, 400)
-arr[1] = 2
-carry = 0
+#Guess on needed size for all inputs.
+arrSize = Int((power / 10) * 4)
+arr = zeros(Int, arrSize)
+#Setting number to num^1
+arr[1] = num
 
-for i = 2 : power
-  #push!(arr, 0)
-  for j = 1 : length(arr) #Something else here?
-    temp = arr[j] * 2
-    if temp > 9
-      #println(temp)
-      arr[j] = temp % 10 + carry
-      carry = 1
-    else
-      arr[j] = temp + carry
-      carry = 0
+function calc(arr)
+  sum = 0
+  carry = 0
+  for i = 2 : power
+    for j = 1 : length(arr)
+      temp = arr[j] * 2
+      if temp > 9
+        arr[j] = temp % 10 + carry
+        carry = 1
+      else
+        arr[j] = temp + carry
+        carry = 0
+      end
     end
   end
-end
-#println(length(arr))
 
-for i = 1 : length(arr)
-  sum += arr[i]
-  print(arr[i])
+  for i = 1 : length(arr)
+    sum += arr[i]
+  end
+  return sum
 end
 
-println("\n$sum")
+println(calc(arr))
 
 
 
