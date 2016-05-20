ccf796f22d5ba9cb5d7692894742be741e59d844
simkn15 <simonknudsen89@gmail.com>
2016-05-20 10:12:23 +0200

    euler5 -> terminal

--- Project-Euler/Solutions-Simon/euler5/euler5.jl
+++ Project-Euler/Solutions-Simon/euler5/euler5.jl
@@ -2,26 +2,26 @@
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 =#
-
 #Answer = 232_792_560
-function divisible()
-  for x = 21:500_000_000
+
+#Terminal: Julia euler5.jl 20 5000000000
+div = parse(Int, ARGS[1])
+limit = parse(Int, ARGS[2])
+
+function divisible(div, limit)
+  for x = div + 1 : limit #start from above div
     evenlyDivis = true
-    if x == 500_000_000
-      println("Done")
-    end
-    for i=2:20
+    for i = 2 : div
       n = x % i
       if !(n == 0)
-        #println("not zero")
         evenlyDivis = false
         break
       end
     end
     if evenlyDivis
-      println("$x is evenly divisible")
+      return x
     end
   end
 end
 
-divisible()
+println(divisible(div, limit))
