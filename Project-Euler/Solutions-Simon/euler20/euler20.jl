8d9dea5ed8cd73b16fa68986060089ea1743a0bb
simkn15 <simonknudsen89@gmail.com>
2016-05-20 13:20:58 +0200

    update

--- Project-Euler/Solutions-Simon/euler20/euler20.jl
+++ Project-Euler/Solutions-Simon/euler20/euler20.jl
@@ -6,7 +6,10 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
 =#
+#Answer: 648
 
+#Terminal: Julia euler20.jl 100
+num = parse(Int, ARGS[1])
 function sum_digits(l)
   sum = 0
   for i in string(l)
@@ -15,11 +18,21 @@ function sum_digits(l)
   return sum
 end
 
+#=
 function fact(n)
   return factorial(big(n))
 end
+=#
 
-println(sum_digits(fact(100)))
+function fact(n)
+  res = big(1)
+  for i = n : -1 : 1
+    res = res * i
+  end
+  return res
+end
+
+println(sum_digits(fact(num)))
 
 
 
