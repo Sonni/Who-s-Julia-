c7fb286cca9eb07989d07196630fe4171e07be58
Asbjørn Mansa Jensen <asbamse@msn.com>
2016-05-23 14:13:52 +0200

    Finally finish making java unds py

--- Project-Euler/Solutions-Simon/euler50/euler50.py
+++ Project-Euler/Solutions-Simon/euler50/euler50.py
@@ -3,9 +3,8 @@ import sys
 limit = int(sys.argv[1])
 
 def getPrimes(size):
-	array = [True for i in range(size+1)]
-	array[0] = False
-	for i in range(2, size+1):
+	array = [True] * (size + 1)
+	for i in range(2, size + 1):
 		if array[i]:
 			j = i + i
 			while j <= size:
@@ -17,16 +16,16 @@ def calc(limit):
   primes = getPrimes(limit)
   maxSeq = 0
   maxPrime = 0
-  for i in range(2, limit):
+  for i in xrange(2, limit):
     sum = 0
     seq = 0
-    for j in range(i, limit):
-      if primes[j] == True:
+    for j in xrange(i, limit):
+      if primes[j]:
         sum += j
         seq += 1
         if sum > limit:
           break
-        if primes[sum] == True:
+        if primes[sum]:
           if seq > maxSeq:
             maxSeq = seq
             maxPrime = sum
