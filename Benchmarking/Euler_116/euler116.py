37641d6bd93227e6b96539960c78c1783f57f4ee
Asbjørn Mansa Jensen <asbamse@msn.com>
2016-05-25 17:35:10 +0200

    Changes

--- Benchmarking/Euler_116/euler116.py
+++ Benchmarking/Euler_116/euler116.py
@@ -5,7 +5,7 @@ def solve(m, n): #m=color block size  n = black box size
     return 0
   solutions = 0
 
-  for i in range(m, n+1):
+  for i in xrange(m, n+1):
     solutions += 1
     solutions += solve(m, n-i)
 
@@ -13,7 +13,7 @@ def solve(m, n): #m=color block size  n = black box size
 
 def s(size, numColors):
   result = 0;
-  for i in range(0, numColors):
+  for i in xrange(0, numColors):
      result += solve(2+i, size)
   return result
 
