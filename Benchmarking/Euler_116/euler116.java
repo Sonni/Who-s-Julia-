5eb0b67902b0e284ce6e8f9bfd354502b11d87c6
Sonni <Sonni@Qt-mc.local>
2016-05-25 17:24:01 +0200

    update

--- Benchmarking/Euler_116/euler116.java
+++ Benchmarking/Euler_116/euler116.java
@@ -1,25 +1,22 @@
-import java.math.BigInteger;
-
 public class euler116 {
     
     public static void main(String[] args) {
         int size = Integer.parseInt(args[0]);
         int numColors = Integer.parseInt(args[1]);
         
-        BigInteger result = BigInteger.valueOf(0);
+        long result = 0l;
         for (int i = 0; i < numColors; i++)
-            result = result.add(BigInteger.valueOf(solve(2+i, size)));
+            result += solve(2+i, size);
         
         System.out.println(result);
-        
     }
     
-    private static int solve(int m, int n)
+    private static long solve(int m, int n)
     {
         if (m > n)
-            return 0;
+            return 0l;
         
-        int solutions = 0;
+        long solutions = 0l;
         
         for (int i = m; i <= n; i++)
         {
