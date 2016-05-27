f7bf12259bc548d977ce60b3e97314966ab91b0a
Asbjørn Mansa Jensen <asbamse@msn.com>
2016-05-27 09:39:22 +0200

    Euler10 py Change

--- Project-Euler/Solutions-Simon/euler10/euler10.py
+++ Project-Euler/Solutions-Simon/euler10/euler10.py
@@ -3,19 +3,19 @@ import sys
 limit = int(sys.argv[1])
 
 def getPrimes(size):
-	array = [True] * (size + 1)
+	array = [True for i in xrange(size + 1)]
 	array[1] = False
 	c = 1
-	for i in range(2, size + 1):
-		if array[i]:
+	for i in xrange(2, size + 1):
+		if array[i] == True:
 			j = i + i
 			while j <= size:
 				array[j] = False
 				j += i
 			c += 1
 	primeArray = []
-	for i in range(2, size + 1):
-		if(array[i]):
+	for i in xrange(2, size + 1):
+		if(array[i])
 			primeArray.append(i)
 	return primeArray
 
