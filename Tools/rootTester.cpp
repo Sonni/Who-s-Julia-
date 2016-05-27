67540d5bf307f8f5cd05ea3d82e95835b827dd43
Sonni <Sonni@Qt-mc.local>
2016-05-27 19:03:39 +0200

    root tester

---
+++ Tools/rootTester.cpp
@@ -0,0 +1,220 @@
+//
+//  main.cpp
+//  Program Tester
+//
+//  Created by Sonni on 11/05/16.
+//  Copyright © 2016 Sonni Jensen. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <dirent.h>
+
+
+std::vector<std::string> split(std::string s, char delim)
+{
+    std::vector<std::string> elems;
+    
+    const char* cstr = s.c_str();
+    unsigned int strLength = (unsigned int)s.length();
+    unsigned int start = 0;
+    unsigned int end = 0;
+    
+    while(end <= strLength)
+    {
+        while(end <= strLength)
+        {
+            if(cstr[end] == delim)
+                break;
+            end++;
+        }
+        
+        elems.push_back(s.substr(start, end - start));
+        start = end + 1;
+        end = start;
+    }
+    
+    return elems;
+}
+
+std::vector<std::string> splitByChar(const std::string& str, int splitLength)
+{
+    int NumSubstrings = str.length() / splitLength;
+    std::vector<std::string> ret;
+    
+    for (auto i = 0; i < NumSubstrings; i++)
+    {
+        ret.push_back(str.substr(i * splitLength, splitLength));
+    }
+    
+    // If there are leftover characters, create a shorter item at the end.
+    if (str.length() % splitLength != 0)
+    {
+        ret.push_back(str.substr(splitLength * NumSubstrings));
+    }
+    
+    
+    return ret;
+}
+
+float getTime(std::string out)
+{
+    std::vector<std::string> a = split(out, '\n');
+    
+    float finalTime = 0.0;
+    
+    for (int i = 0; i < a.size(); i++)
+    {
+        int counter = 0;
+        std::string timeKind = "";
+        
+        for(std::string::iterator it = a[i].begin(); it != a[i].end(); ++it) {
+            timeKind += *it;
+            counter++;
+            if (counter == 3)
+                break;
+        }
+        
+        if (!timeKind.compare("use"))
+        {
+            std::string time = a[i].substr(5, a[i].length());
+            
+            finalTime += std::stof(time);
+            
+        }
+        
+        if (!timeKind.compare("sys"))
+        {
+            std::string time = a[i].substr(4, a[i].length());
+            
+            finalTime += std::stof(time);
+            
+        }
+        
+        
+    }
+    
+    return finalTime;
+}
+
+std::string readOutput(std::string path)
+{
+    std::ifstream infile(path);
+    std::string result;
+    
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        result += line + "\n";
+    }
+    infile.close();
+    
+    return result;
+}
+
+std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer, 128, pipe.get()) != NULL)
+            result += buffer;
+    }
+    return result;
+}
+
+void doTest(std::string title, const char* command)
+{
+    int loopCount = 5;
+    
+    float sum = 0.0;
+    float median = 0.0;
+    
+    std::cout << title << "" << std::endl;
+    for (int i = 0; i < loopCount; i++)
+    {
+        //std::string out = exec(command);
+        system(command);
+        
+        std::string outString = readOutput("out.txt");
+        float time = getTime(outString);
+        sum += time;
+        if (i == loopCount/2)
+            median = time;
+        
+        std::cout << time << std::endl;
+    }
+    
+    std::cout << "Average time is: " << sum / loopCount << " : Median is: " << median << std::endl;
+    std::cout << "____________________________________" << std::endl;
+}
+
+void doFolder(std::string path)
+{
+    std::ifstream infile(path + "/script.sh");
+    int counter = 0;
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        
+        std::string language = split(line, ' ')[0];
+        std::string programName = split(line, ' ')[1];
+        std::string arguments = "";
+        if (!language.compare("/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia"))
+            language = "Julia";
+        
+        if (language[0] == '.' && language[1] == '/')
+        {
+            language = "C++";
+            programName = split(line, ' ')[0];
+            std::vector<std::string> arg = split(line, ' ');
+            for (unsigned int i = 1; i < arg.size(); i++)
+                arguments += arg[i] + " ";
+        }
+        else
+        {
+            std::vector<std::string> arg = split(line, ' ');
+            for (unsigned int i = 2; i < arg.size(); i++)
+                arguments += arg[i] + " ";
+        }
+        
+        
+        std::string command = "(time -p " + line + ") 2> out.txt";
+        
+        doTest(language + " : " + programName + " " + arguments, command.c_str());
+        counter++;
+    }
+    infile.close();
+}
+
+int main(int argc, const char * argv[])
+{
+    const char* PATH = ".";
+    
+    DIR *dir = opendir(PATH);
+    
+    struct dirent *entry = readdir(dir);
+    
+    while (entry != NULL)
+    {
+        if (entry->d_type == DT_DIR)
+            if (std::strcmp(entry->d_name, ".") && std::strcmp(entry->d_name, ".."))
+            {
+                std::cout << entry->d_name << " ";
+                doFolder(entry->d_name);
+            }
+        
+        entry = readdir(dir);
+    }
+    
+    closedir(dir);
+    
+    return 0;
+}
