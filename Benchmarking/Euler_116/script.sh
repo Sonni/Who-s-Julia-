7f696fa34e907b865e9bfa1e67e47e5bd253b22e
Sonni <Sonni@Qt-mc.local>
2016-05-29 08:04:29 +0200

    116 benchmark

---
+++ Benchmarking/Euler_116/script.sh
@@ -0,0 +1,44 @@
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 45
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 46
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 47
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 48
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 49
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 50
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 51
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 52
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 53
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 54
+/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia euler116.jl 55
+./euler116 45
+./euler116 46
+./euler116 47
+./euler116 48
+./euler116 49
+./euler116 50
+./euler116 51
+./euler116 52
+./euler116 53
+./euler116 54
+./euler116 55
+java euler116 45
+java euler116 46
+java euler116 47
+java euler116 48
+java euler116 49
+java euler116 50
+java euler116 51
+java euler116 52
+java euler116 53
+java euler116 54
+java euler116 55
+python euler116.py 45
+python euler116.py 46
+python euler116.py 47
+python euler116.py 48
+python euler116.py 49
+python euler116.py 50
+python euler116.py 51
+python euler116.py 52
+python euler116.py 53
+python euler116.py 54
+python euler116.py 55
