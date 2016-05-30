729c64afe6189658e9fef397cf035b81b5e7e65d
simkn15 <simonknudsen89@gmail.com>
2016-05-30 15:43:39 +0200

    upd

--- Benchmarking/todo/euler77/script.sh
+++ Benchmarking/todo/euler77/script.sh
@@ -1,4 +1,4 @@
-julia euler77/euler77.jl 5000 1000000
-python euler77/euler77.py 5000 1000000
-java euler77/euler77 5000 1000000
-./euler77/euler77 5000 1000000
+julia euler77/euler77.jl 5000
+python euler77/euler77.py 5000
+java euler77/euler77 5000
+./euler77/euler77 5000
